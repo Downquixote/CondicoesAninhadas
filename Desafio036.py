1af7936d1153bed7bc8800c1fc48de7aa647ded9
Lucas Brandão <122632969+Downquixote@users.noreply.github.com>
2023-06-20 18:25:04 -0300

    Desafio036.py

--- Desafio036.py
+++ Desafio036.py
@@ -1,6 +1,7 @@
 # Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa.
 # O programa vai perguntar o VALOR DA CASA, o SALÁRIO do comprador e em QUANTOS ANOS ele vai pagar.
 # Calcule o valor da prestação mensal, sabendo que ela não deverá exceder 30% do salário ou então o empréstimo será negado.
+
 valor = float(input('Qual o valor do imóvel? '))
 salario = float(input('Qual o valor da sua renda mensal? '))
 anos = int(input('Quantos anos irá pagar? '))
